5715d9b3325c732f4c90d0eb5102851be49ed73e
harris <harrisleesh@gmail.com>
2020-02-13 23:28:42 +0900

    LottoGame
    First Commit

--- src/TryLottoDto.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashSet;
-
-public class TryLottoDto {
-    public HashSet<Integer> sixNum;
-
-    public TryLottoDto(HashSet<Integer> sixNum) {
-        this.sixNum = sixNum;
-    }
-}
